9e7494cf2c9ee2efc9a455c2023a52f19a6af8e0
Andrii <110056592+andriimonastyrskyi@users.noreply.github.com>
2023-01-21 11:16:44 +0200

    nowork

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,58 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryRefs = document.querySelector(".gallery");
+console.log(galleryRefs);
+
+function addGalleryItems(galleryItems) {
+  return galleryItems
+    .map(
+      ({ preview, original, description }) =>
+        `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+    )
+    .join("");
+  // console.log(galleryItems);
+}
+
+// galleryRefs.innerHTML = galleryItems;
+
+galleryRefs.insertAdjacentHTML("beforeend", addGalleryItems(galleryItems));
+
+galleryRefs.addEventListener("click", openModal);
+
+function openModal(event) {
+  event.preventDefault();
+  if (!event.target.classList.contains("gallery__image")) {
+    return;
+  }
+
+  const instance = basicLightbox.create(
+    `<img src="${event.target.dataset.source}" width="800" height="600">`,
+    {
+      onOpen: (instance) => {
+        window.addEventListener("keydown", closeModal);
+      },
+      onClose: (instance) => {
+        window.removeEventListener("keydown", closeModal);
+      },
+    }
+  );
+
+  function closeModal(event) {
+    if (event.code === "Escape") {
+      instance.close();
+    }
+  }
+  instance.show();
+}
